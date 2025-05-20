2e85e2a763537b1967eb2d6e1e9ca9163624b444
he1kor <helkor.rightway@gmail.com>
2025-05-20 05:27:05 +0300

    feat: use random seed check box

--- src/ui/generation_ui.cpp
+++ src/ui/generation_ui.cpp
@@ -1,25 +1,38 @@
 #include "generation_ui.h"
 #include "imgui.h"
 #include "map_render.h"
+#include <iostream>
+#include <string>
 
 
 GenerationUI::GenerationUI(Generation *generationPtr, MapRenderer *mapRendererPtr){
     this->generationPtr = generationPtr;
     this->mapRendererPtr = mapRendererPtr;
 }
-void GenerationUI::build()
-{
+void GenerationUI::build(){
     ImGui::Text("Generation");
     ImGui::Dummy(ImVec2(0, 5));
     
     mapRendererPtr->render();
 
     ImGui::Dummy(ImVec2(0, 5));
+    
     ImGui::Text("Seed");
     ImGui::SetNextItemWidth(-1);
-    ImGui::InputScalar("##Seed", ImGuiDataType_S64, &generationPtr->seed);
+    
+    
+    if (generationPtr->is_seed_random) {
+        ImGui::BeginDisabled();
+        ImGui::InputScalar("##Seed", ImGuiDataType_S64, &(generationPtr->seed), NULL, NULL, NULL, ImGuiInputTextFlags_ReadOnly);   
+        ImGui::EndDisabled();
 
-
+        if (ImGui::IsMouseHoveringRect(ImGui::GetItemRectMin(), ImGui::GetItemRectMax())) {
+        ImGui::SetTooltip("Seed will randomize on generation");
+    }
+    } else {
+        ImGui::InputScalar("##Seed", ImGuiDataType_S64, &(generationPtr->seed));
+    }
+    ImGui::Checkbox("Use random##use random seed", &(generationPtr->is_seed_random));
 
     float available_space = ImGui::GetContentRegionAvail().y - 40.0f - ImGui::GetStyle().ItemSpacing.y;
     ImGui::Dummy(ImVec2(0, available_space));
