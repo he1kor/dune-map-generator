8b0d7f2a54fe8a76e7df6d49e8b6eaf586fcb115
he1kor <helkor.rightway@gmail.com>
2025-05-20 05:24:35 +0300

    feat: colors header

---
+++ include/global/colors.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "imgui.h"
+
+constexpr auto PINK_HARD = ImVec4(0.6f, 0.4117647f, 0.411764f, 1.00f);
+constexpr auto PINK_SOFT = ImVec4(0.9020f, 0.8392f, 0.8235f, 1.00f);
+constexpr auto PINK_ULTRA_SOFT = ImVec4(0.8824f, 0.7882f, 0.7882f, 1.00f);
+
+constexpr auto RED_BASIC = ImVec4(0.4745f, 0.2549f, 0.2549f, 1.00f);
+constexpr auto RED_EXTRA = ImVec4(0.6118f, 0.3137f, 0.3137f, 1.00f);
+constexpr auto RED_ULTRA = ImVec4(0.6941f, 0.4118f, 0.4118f, 1.00f);
+
+constexpr auto VARIANT_BASIC = ImVec4(0.3137f, 0.5137f, 0.5137f, 1.00f);
+constexpr auto VARIANT_BASIC_TRANSPARENT = ImVec4(0.3137f, 0.5137f, 0.5137f, 0.50f);
+constexpr auto VARIANT_EXTRA = ImVec4(0.3804f, 0.6314f, 0.6314f, 1.00f);
+constexpr auto VARIANT_ULTRA = ImVec4(0.3373f, 0.6549f, 0.6549f, 1.00f);
+
+constexpr auto VARIANT2_BASIC = ImVec4(0.5647f, 0.3490f, 0.1490f, 1.00f);
+constexpr auto VARIANT2_BASIC_TRANSPARENT = ImVec4(0.5647f, 0.3490f, 0.1490f, 0.50f);
+constexpr auto VARIANT2_EXTRA = ImVec4(0.6510f, 0.4039f, 0.1725f, 1.00f);
+constexpr auto VARIANT2_ULTRA = ImVec4(0.7490f, 0.5020f, 0.1961f, 1.00f);
+constexpr auto VARIANT2_ULTRA_TRANSPARENT = ImVec4(0.7490f, 0.5020f, 0.1961f, 0.70f);
+
+constexpr auto SOFT_BROWN = ImVec4(0.27059f, 0.2f, 0.2f, 1.00f);
+constexpr auto MEDIUM_BROWN = ImVec4(0.2118f, 0.1569f, 0.1569f, 1.00f);
+constexpr auto HARD_BROWN = ImVec4(0.16862f, 0.129411f, 0.12549f, 1.00f);
