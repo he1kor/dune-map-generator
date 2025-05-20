2e5b77c6dca007ed4d9bbc2042cab93eb48bc24e
he1kor <helkor.rightway@gmail.com>
2025-05-20 05:25:01 +0300

    feat: file button styles

--- src/ui/template_picker_ui.cpp
+++ src/ui/template_picker_ui.cpp
@@ -2,6 +2,7 @@
 #include "template_picker_ui.h"
 #include <iostream>
 #include "file_pick_button.h"
+#include "colors.h"
 
 TemplatePickerUI::TemplatePickerUI(TemplatePicker *templatePickerPtr){
     this->templatePickerPtr = templatePickerPtr;
@@ -13,15 +14,28 @@ void TemplatePickerUI::build(){
     ImGui::Dummy(ImVec2(0, 5));
 
 
+
     buildFilePickButton("Template:", "Select file...", "template");
+
+
     ImGui::Dummy(ImVec2(0, 10));
+
+    ImGui::PushStyleColor(ImGuiCol_Button, VARIANT_BASIC);
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, VARIANT_EXTRA);
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive, VARIANT_ULTRA);
     buildFilePickButton("Tileset config:", "Select file...", "config");
 
+    ImGui::PopStyleColor(3);
 
     float available_space = ImGui::GetContentRegionAvail().y - 40.0f - ImGui::GetStyle().ItemSpacing.y;
     ImGui::Dummy(ImVec2(0, available_space));
 
+    ImGui::PushStyleColor(ImGuiCol_Button, VARIANT2_BASIC);
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, VARIANT2_EXTRA);
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive, VARIANT2_ULTRA);
     if (ImGui::Button("Generate", ImVec2(-1, 40))) {
         // Generate action
     }
+    ImGui::PopStyleColor(3);
+
 }
